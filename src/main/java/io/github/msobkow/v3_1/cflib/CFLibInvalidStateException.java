94b8e25cb3b9cad46cc134c13d489bd04d9185b8
Mark Sobkow <mark.sobkow@gmail.com>
2026-02-21 18:51:59 -0600

    2026-02-21 18:50 CST Minor tweaks to the configuration listener in hopes of stabilizing the code base.

---
+++ src/main/java/io/github/msobkow/v3_1/cflib/CFLibInvalidStateException.java
@@ -0,0 +1,216 @@
+/*
+ *	Mark's Code Fractal CFLib 3.1 Common Library
+ *
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *
+ *	This file is part of Mark's Code Fractal CFLib.
+ *
+ *	Mark's Code Fractal CFLib is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *
+ *	Mark's Code Fractal CFLib is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Mark's Code Fractal CFLib is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Library General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFLib.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package io.github.msobkow.v3_1.cflib;
+
+import io.github.msobkow.v3_1.cflib.inz.Inz;
+
+/**
+ * CFLibEmptyArgumentException is thrown when an argument is null or empty.
+ */
+public class CFLibInvalidStateException extends CFLibArgumentException {
+
+	// Inherited constructor patterns
+
+	public CFLibInvalidStateException(
+		String enMsg,
+		String xMsg )
+	{
+		super(enMsg, xMsg);
+	}
+
+	public CFLibInvalidStateException(
+		String enMsg,
+		String xMsg,
+		Throwable th)
+	{
+			super(enMsg, xMsg, th);
+	}
+
+//	public CFLibInvalidStateException(
+//		Class<?> throwingClass,
+//		String methName,
+//		String enMsg,
+//		String xMsg )
+//	{
+//		super(throwingClass, methName, enMsg, xMsg);
+//	}
+
+//	public CFLibInvalidStateException(
+//		Class<?> throwingClass,
+//		String methName,
+//		String enMsg,
+//		String xMsg,
+//		Throwable th )
+//	{
+//		super(throwingClass, methName, enMsg, xMsg, th);
+//	}
+
+	public CFLibInvalidStateException(
+	 	Class<?> throwingClass,
+	 	String methName,
+	 	int argNo,
+	 	String argName,
+	 	String enMsg,
+	 	String xMsg )
+	{
+	 	super(throwingClass, methName, argNo, argName, enMsg, xMsg);
+	}
+
+	public CFLibInvalidStateException(
+	 	Class<?> throwingClass,
+	 	String methName,
+	 	int argNo,
+	 	String argName,
+	 	String enMsg,
+	 	String xMsg,
+	 	Throwable th )
+	{
+	 	super(throwingClass, methName, argNo, argName, enMsg, xMsg, th);
+	}
+
+	public CFLibInvalidStateException(
+		String enFieldName,
+		String xFieldName,
+		String enMsg,
+		String xMsg )
+	{
+		super(enFieldName, xFieldName, enMsg, xMsg);
+	}
+
+//	public CFLibInvalidStateException(
+//		String enFieldName,
+//		String xFieldName,
+//		String methName,
+//		String enMsg,
+//		String xMsg )
+//	{
+//		super(enFieldName, xFieldName, methName, enMsg, xMsg);
+//	}
+
+//	public CFLibInvalidStateException(
+//		String enFieldName,
+//		String xFieldName,
+//		String methName,
+//		String enMsg,
+//		String xMsg,
+//		Throwable th )
+//	{
+//		super(enFieldName, xFieldName, methName, enMsg, xMsg, th);
+//	}
+
+	public CFLibInvalidStateException(
+	 	String enFieldName,
+		String xFieldName,
+	 	String methName,
+	 	int argNo,
+	 	String argName,
+	 	String enMsg,
+		String xMsg )
+	{
+	 	super(enFieldName, xFieldName, methName, argNo, argName, enMsg, xMsg);
+	}
+
+	public CFLibInvalidStateException(
+	 	String enFieldName,
+		String xFieldName,
+	 	String methName,
+	 	int argNo,
+	 	String argName,
+	 	String enMsg,
+	 	String xMsg,
+	 	Throwable th )
+	{
+	 	super(enFieldName, xFieldName, methName, argNo, argName, enMsg, xMsg, th);
+	}
+
+	// Custom/most-often-used constructors
+
+	public CFLibInvalidStateException(
+		Class<?> throwingClass,
+		String methName,
+		String enReason,
+		String xReason )
+	{
+		super(String.format(Inz.s("cflib.CFLibInvalidStateException.TcmnMsg"),
+				throwingClass.getName()	+ ( ( ( methName != null ) && !methName.isEmpty()) ? "." + methName + "()" : "" ),
+				enReason),
+			String.format(Inz.x("cflib.CFLibInvalidStateException.TcmnMsg"),
+				throwingClass.getName()	+ (( methName != null && !methName.isEmpty()) ? ("." + methName + "()") : "" ),
+				xReason));
+	}
+
+	public CFLibInvalidStateException(
+		Class<?> throwingClass,
+		String methName,
+		String enReason,
+		String xReason,
+		Throwable th )
+	{
+		super(String.format(Inz.s("cflib.CFLibInvalidStateException.TcmnMsg"),
+				throwingClass.getName()	+ ( ( ( methName != null ) && !methName.isEmpty()) ? "." + methName + "()" : "" ),
+				enReason),
+		String.format(Inz.x("cflib.CFLibInvalidStateException.TcmnMsg"),
+				throwingClass.getName()	+ (( methName != null && !methName.isEmpty()) ? ("." + methName + "()") : "" ),
+				xReason),
+			th);
+	}
+
+	public CFLibInvalidStateException(
+		String enFieldName,
+		String xFieldName,
+		String methName,
+		String enReason,
+		String xReason )
+	{
+		super(String.format(Inz.s("cflib.CFLibInvalidStateException.FldMsg"),
+				enFieldName + ( ( methName != null && !methName.isEmpty()) ? "." + methName + "()"	: "" ),
+				enReason),
+		String.format(Inz.x("cflib.CFLibInvalidStateException.FldMsg"),
+				enFieldName + ( ( methName != null && !methName.isEmpty()) ? "." + methName + "()"	: "" ),
+				xReason));
+	}
+
+	public CFLibInvalidStateException(
+		String enFieldName,
+		String xFieldName,
+		String methName,
+		String enReason,
+		String xReason,
+		Throwable th )
+	{
+		super(String.format(Inz.s("cflib.CFLibInvalidStateException.FldMsg"),
+				enFieldName + ( ( methName != null && !methName.isEmpty()) ? "." + methName + "()"	: "" ),
+				enReason),
+		String.format(Inz.x("cflib.CFLibInvalidStateException.FldMsg"),
+				enFieldName + ( ( methName != null && !methName.isEmpty()) ? "." + methName + "()"	: "" ),
+				xReason),
+				th );
+	}
+}
